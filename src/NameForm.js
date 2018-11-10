901659badfaf24a04307a575339f8bacde4ae5dd
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-10 12:56:49 -0500

    Adding NameForm component

---
+++ src/NameForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './NameForm.css';
+
+class NameForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        alert(`A name has been submitted! ${this.state.value}`);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>Name:</label>
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
+export default NameForm;
